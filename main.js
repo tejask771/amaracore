6c68da322170f26d7556ac2a4c842880765e794b
Your Name <tejask771@gmail.com>
2021-11-12 12:16:34 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function getData(){
+    const url = "https://60ac9dff9e2d6b0017457815.mockapi.io/ag/contacts";
+    fetch(url).then((res)=>{
+        return res.json();
+    }).then((mydata)=>{
+        console.log(mydata);
+        buildTable(mydata);
+    }).catch((err) => {
+        console.error(err);
+    });
+}
+
+function buildTable(data){
+    const table = document.getElementById('myTable');
+    for (let i = 0; i < data.length; i++){
+        const row = `<tr>
+            <td>${data[i].id}</td>
+            <td>${data[i].avatar}</td>
+            <td>${data[i].first_name}</td>
+            <td>${data[i].last_name}</td>
+            <td>${data[i].phone}</td>
+            <td>${data[i].email}</td>
+            <td>${data[i].company}</td>
+            <td>${data[i].job_title}</td>
+        </tr>`;
+        table.innerHTML += row;
+    }
+    // for(let j of data) {
+
+    // }
+    // for(let i in data) {
+
+    // }
+}
+
+getData();
