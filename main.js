62ea1202151d3459057a005811280e8f33b3bca5
Tejas Kothawade <tejask771@gmail.com>
2021-11-12 12:47:23 +0530

    Updated comments

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-function getData(){
+// function to fetch data
+function getData() {
     const url = "https://60ac9dff9e2d6b0017457815.mockapi.io/ag/contacts";
-    fetch(url).then((res)=>{
+    fetch(url).then((res) => {  //fetched API using fetch method
         return res.json();
-    }).then((mydata)=>{
+    }).then((mydata) => {       //stored result into mydata oject
         console.log(mydata);
         buildTable(mydata);
-    }).catch((err) => {
+    }).catch((err) => {         // hadnled the error if an occurs 
         console.error(err);
     });
 }
 
-function buildTable(data){
+//Function to add data to the table
+function buildTable(data) {
     const table = document.getElementById('myTable');
-    for (let i = 0; i < data.length; i++){
+    for (let i = 0; i < data.length; i++) {  //for loop to iterate json object
         const row = `<tr>
             <td>${data[i].id}</td>
             <td>${data[i].avatar}</td>
@@ -23,7 +25,7 @@ function buildTable(data){
             <td>${data[i].company}</td>
             <td>${data[i].job_title}</td>
         </tr>`;
-        table.innerHTML += row;
+        table.innerHTML += row;             //added html for the table
     }
 }
 
